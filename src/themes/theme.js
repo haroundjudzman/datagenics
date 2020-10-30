a0e99e5ac264c22daa4818b263494632333efe18
Haroun Djudzman <haroun.djudzman@gmail.com>
2020-10-30 11:44:11 +0100

    add styled components

---
+++ src/themes/theme.js
@@ -0,0 +1,34 @@
+export default {
+  fonts: {
+    head: "Raleway, sans-serif",
+    body: "Montserrat', sans-serif",
+    code: "Roboto Mono, monospace",
+  },
+  colors: {
+    main1: "hsl(204, 79%, 20%)",
+    main2: "hsl(202, 59%, 48%)",
+    main3: "hsl(49, 86%, 46%)",
+    dark1: "hsl(210, 26%, 15%)",
+    light1: "hsl(240, 100%, 98%)"
+  },
+  breakpoints: {
+    mobile: "only screen and (max-width: 50rem)",
+    tablet: "only screen and (max-width: 65rem)",
+  },
+  spacings: {
+    xxSmall: ".25rem",
+    xSmall: ".5rem",
+    small: "1rem",
+    medium: "2rem",
+    large: "3rem",
+    xLarge: "4rem",
+    xxLarge: "6rem",
+  },
+  animations: {
+    button: "box-shadow 0.3s ease",
+    link: "color 0.2s ease",
+  },
+  shadows: {
+    shadow1: "0px 5px 20px rgba(30, 30, 31, 0.05)",
+  },
+}
